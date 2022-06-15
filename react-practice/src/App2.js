c3bd8305c2f44c853c8190c096aa3b0ab7c4aefe
hana1203 <hanaindec@gmail.com>
2022-06-15 23:44:11 +0900

    implementing ipnut for converting usd to coin cost

--- react-practice/src/App2.js
+++ react-practice/src/App2.js
@@ -4,6 +4,20 @@ function App2() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
 
+  const [currentCoin, setCurrentCoin] = useState([]);
+  let splitCost = currentCoin.split("$");
+  const currentCoinCost = splitCost[1];
+  const [dollars, setDollars] = useState();
+
+  const onChange = (event) => {
+    setDollars(event.target.value);
+  };
+
+  const saveCurrentCoin = (event) => {
+    setCurrentCoin(event.target.value);
+  };
+  // console.log(currentCoin.split("$")[1]);
+
   //컴포넌트가 초기에 처음으로 렌더될때 API 가져오는 함수를 실행시키기
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
@@ -16,14 +30,41 @@ function App2() {
   return (
     <div>
       <h1>The Coins ({coins.length})</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={saveCurrentCoin}>
+          {coins.map((coin) => (
+            <option>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}
+            </option>
+          ))}
+        </select>
+      )}
+      {/* <ul>
         {coins.map((coin) => (
           <li>
             {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}
           </li>
         ))}
-      </ul>
+      </ul> */}
+      {/* select만들어서 코인 선택하고 코인가격 -usd 변환하게 하기 */}
+      <h2>how many coins can I buy?</h2>
+      <input
+        value={dollars}
+        onChange={onChange}
+        placeholder="Enter USD amount"
+        type="number"
+        id="dollars"
+      ></input>
+      {/* {coins.map((coin) => (
+        <input placeholder={coin.name}></input>
+      ))} */}
+      <input
+        value={dollars / currentCoinCost}
+        placeholder={currentCoin}
+        disabled
+      ></input>
     </div>
   );
 }
