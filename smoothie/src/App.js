21a1c56f297f7d43ff0632f203f54fee44845a6b
hana1203 <hanaindec@gmail.com>
2022-06-30 23:41:02 +0900

    amended to jsx

--- smoothie/src/App.js
+++ smoothie/src/App.js
@@ -1,4 +1,35 @@
 import "./App.css";
+import { useState } from "react";
+
+function Menu({ icon, name, storeMenu }) {
+  // const [currentMenu, setCurrentMenu] = useState();
+  // const onClick = (event) => {
+  //   setCurrentMenu(event.target.textContent);
+  // };
+  // console.log(currentMenu);
+
+  return (
+    <div onClick={storeMenu} className="menu">
+      <div>{icon}</div>
+      <div>{name} </div>
+    </div>
+  );
+}
+
+function Selected() {
+  const [storedMenu, setStoredMenu] = useState();
+  const storeMenu = (event) => {
+    setStoredMenu(event.target.textContent);
+  };
+  console.log(storedMenu);
+  return (
+    <div className="selected">
+      <div className="selected_menu">selected menu</div>
+      <div className="selected_menu_icon">{storedMenu}</div>
+      <div className="selected_menu_name">{storedMenu}</div>
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -6,50 +37,68 @@ function App() {
       <h1>Organic Smoothie Bar</h1>
       <h1>👩🏻‍🌾</h1>
       <h2>Customize Your Organic Smoothie</h2>
-      <section className="menu_container"></section>
-      <div className="menu">
-        <div>🥬</div>
-        <div>Kale</div>
-      </div>
-      <div className="menu">
-        <div>🥕</div>
-        <div>Carrot</div>
-      </div>
-      <div className="menu">
-        <div>🥝</div>
-        <div>Kiwi</div>
-      </div>
-      <div className="menu">
-        <div>🍍</div>
-        <div>Pineapple</div>
-      </div>
-      <div className="menu">
-        <div>🫐</div>
-        <div>Blueberry</div>
-      </div>
-      <div className="menu">
-        <div>🍓</div>
-        <div>Strawberry</div>
-      </div>
-      <div className="menu">
-        <div>🍌</div>
-        <div>Banana</div>
-      </div>
-      <div className="menu">
-        <div>🍋</div>
-        <div>Lemon</div>
-      </div>
-      <div className="menu">
-        <div>🍊</div>
-        <div>Orange</div>
-      </div>
+      <section className="menu_container">
+        <Menu icon="🥬" name="Kale"></Menu>
+        <Menu icon="🥕" name="Carrot"></Menu>
+        <Menu icon="🥝" name="Kiwi"></Menu>
+        <Menu icon="🍍" name="Pineapple"></Menu>
+        <Menu icon="🫐" name="Blueberry"></Menu>
+        <Menu icon="🍓" name="Strawberry"></Menu>
+        <Menu icon="🍌" name="Banana"></Menu>
+        <Menu icon="🍋" name="Lemon"></Menu>
+        <Menu icon="🍊" name="Orange"></Menu>
+      </section>
+      {/* html 다 가져올 필요없고 컴포넌트로 만들어서 넣기  */}
+      {/* <section className="menu_container">
+        <div className="menu">
+          <div>🥬</div>
+          <div>Kale</div>
+        </div>
+        <div className="menu">
+          <div>🥕</div>
+          <div>Carrot</div>
+        </div>
+        <div className="menu">
+          <div>🥝</div>
+          <div>Kiwi</div>
+        </div>
+        <div className="menu">
+          <div>🍍</div>
+          <div>Pineapple</div>
+        </div>
+        <div className="menu">
+          <div>🫐</div>
+          <div>Blueberry</div>
+        </div>
+        <div className="menu">
+          <div>🍓</div>
+          <div>Strawberry</div>
+        </div>
+        <div className="menu">
+          <div>🍌</div>
+          <div>Banana</div>
+        </div>
+        <div className="menu">
+          <div>🍋</div>
+          <div>Lemon</div>
+        </div>
+        <div className="menu">
+          <div>🍊</div>
+          <div>Orange</div>
+        </div>
+      </section> */}
+
       <section className="order_container">
-        <div className="selected">
+        {/* <div className="selected">
           <div>Selected ingredients</div>
           <div className="selected_menu"></div>
-          <div className="selected_menu_image"></div>
+          <div className="selected_menu_icon"></div>
           <div className="selected_menu_name"></div>
-        </div>
+        </div> */}
+
+        <div>Selected ingredients</div>
+        <Selected></Selected>
+
         <div className="order_detail">
           <div className="order_size">
             <span className="label">Size</span>
